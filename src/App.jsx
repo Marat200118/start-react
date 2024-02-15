68965c1792a603325729d33a9ab9014af6bfd07e
Marat200118 <marat.samigullin1@gmail.com>
2024-02-15 16:16:47 +0100

    second task completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
+import { Bio } from "./Bio";
+import Meals, { List } from "./Meals";
+
 function App() {
   return (
     <article>
       <h2>Hi, I am Jeremy Robinson</h2>
-      <p>I was born in 1999 and I live in Laarne</p>
-      <section>
-        <h3>My favourite meals</h3>
-        <ul>
-          <li>Spaghetti</li>
-          <li>French fries</li>
-          <li>Roasted chicken</li>
-        </ul>
-      </section>
+      <Bio />
+      <Meals />
+      <List />
     </article>
   );
 }
---
+++ src/Bio.jsx
@@ -0,0 +1,1 @@
+export const Bio = () => <p>I was born in 1999 and I live in Laarne</p>;
